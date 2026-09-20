398edd8b0350ade7637c494822db0d184aee41ed
Shreyash Singh <shreyashsingh9717@gmail.com>
2026-09-20 13:37:31 +0530

    fixed vercel dynamic api error

--- src/app/api/user/profile/route.ts
+++ src/app/api/user/profile/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth/next";
@@ -43,3 +44,4 @@ export async function GET() {
     return NextResponse.json({ message: "Error" }, { status: 500 });
   }
 }
+
